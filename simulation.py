6ff61da3b16cd4e2fdfbc1121d5bebca3620a087
Benedikt00 <b.simbuerger@aon.at>
2022-09-24 16:31:34 +0200

    v1

---
+++ simulation.py
@@ -0,0 +1,35 @@
+import pygame
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+points = []
+
+for i in range(8):
+    for j in range(8):
+        for k in range(8):
+            points.append([i, j, k, 0])
+
+plt.ion()
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+
+xdata = []
+ydata = []
+zdata = []
+for p in points:
+    xdata.append(p[0])
+    ydata.append(p[1])
+    zdata.append(p[2])
+
+color = ["white" for i in points]
+
+ax.scatter3D(xdata, ydata, zdata, color = color)
+
+plt.show()
+
+color = ["red" for i in points]
+plt.draw()
+
+
+
